7c1f0684f3ba7201ee1930b93ca6aa6fde66ebc4
Mohiyudeen Bajal <mohiyudeen.bajal@nytimes.com>
2017-05-25 13:23:10 -0400

    add mouse

--- app.js
+++ app.js
@@ -1,12 +1,66 @@
 var canvas = document.getElementById('game');
 var c = canvas.getContext("2d");
 
-c.fillStyle = 'rgba(255, 0 , 0 ,0.3)';
+var tileFront='rgb(153, 153, 153)'
+var tileBack='rgb(204, 204, 204)'
+var fontColor = 'black';
+var tileSize = 95;
+var tileLocations = [];
+c.font = "28pt Tahoma";
 
-for(var j=0; j<4; j++){
-  for(var i=0; i<4; i++){
-    c.fillRect(10 + (i*95 + i*10), 10 + (j*95 + j*10), 95,95);
+function tilexy(x,y){
+  this.x = x;
+  this.y = y;
+  clicked=false;
+
+}
+
+window.addEventListener('mousedown',
+function(event){
+  //console.log(event);
+  //flipTile(event.x, event.y);
+  isTileClicked(event.x, event.y)
+});
+
+function initBoard() {
+  c.fillStyle = tileFront;
+  for(var j=0; j<4; j++){
+    for(var i=0; i<4; i++){
+      tileLocations.push(new tilexy(10 + (i*tileSize + i*10), 10 + (j*tileSize + j*10)))
+      c.fillRect(10 + (i*tileSize + i*10), 10 + (j*tileSize + j*10), tileSize ,tileSize);
+    }
+  }
+  //console.log(tileLocations)
+}
+
+function flipTile(tileNumber) {
+  if(tileLocations[tileNumber].clicked == true) {
+    c.fillStyle = tileBack;
+  } else {
+    c.fillStyle = tileFront;
+  }
+  c.fillRect(tileLocations[tileNumber].x,tileLocations[tileNumber].y,tileSize,tileSize);
+  c.fillStyle = fontColor;
+  if(tileLocations[tileNumber].clicked == true) {
+    c.fillText(  tileNumber, tileLocations[tileNumber].x + tileSize/2-10, tileLocations[tileNumber].y  + tileSize/2+10);
   }
 }
 
-console.log("Hey")
+function isTileClicked(px, py){
+    for(var i=0; i < tileLocations.length; i++){
+      if( (px > tileLocations[i].x  && px < tileLocations[i].x + tileSize) && (py>tileLocations[i].y && py < tileLocations[i].y + tileSize) ){
+          //console.log("Tile "+i+" Clicked!")
+          tileLocations[i].clicked = !tileLocations[i].clicked; //Toggle the clicked state
+          flipTile(i);
+      }
+    }
+}
+
+
+
+
+
+
+
+
+initBoard();
